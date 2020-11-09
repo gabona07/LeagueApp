b29ee6cbfcb2ef043477a14667f9368e5609128b
BendeguzTajti <bendeguz9ctajti@gmail.com>
2020-11-09 12:46:52 +0100

    refactor navigation setup

--- app/src/main/java/com/example/leagueapp/MainActivity.java
+++ app/src/main/java/com/example/leagueapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
@@ -15,18 +16,27 @@ import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarLayout appBarLayout;
     private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        appBarInit();
+        MaterialToolbar toolbar = findViewById(R.id.appToolbar);
+        setSupportActionBar(toolbar);
+        NavHostFragment host = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.navHostFragment);
+        if (host != null) {
+            navController = host.getNavController();
+            AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.championsFragment)
+                    .build();
+            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        AppBarLayout appBarLayout = findViewById(R.id.appBar);
         appBarLayout.setExpanded(true);
         return super.onCreateOptionsMenu(menu);
     }
@@ -39,16 +49,5 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onSupportNavigateUp() {
         return navController.navigateUp() || super.onSupportNavigateUp();
-    }
-
-    private void appBarInit() {
-        appBarLayout = findViewById(R.id.appBar);
-        MaterialToolbar toolbar = findViewById(R.id.appToolbar);
-        setSupportActionBar(toolbar);
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHostFragment);
-        if (navHostFragment != null) {
-            navController = navHostFragment.getNavController();
-            NavigationUI.setupActionBarWithNavController(this, navController);
-        }
     }
 }
